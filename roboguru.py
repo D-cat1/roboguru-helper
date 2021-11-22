f040de249610c879e82c39a551a3f4023af3122a
D-cat1 <72681912+D-cat1@users.noreply.github.com>
2021-11-22 23:51:43 +0700

    Update roboguru.py

--- roboguru.py
+++ roboguru.py
@@ -4,6 +4,7 @@ import urllib.parse as asik
 import html2text
 import imgkit
 import string
+from pyvirtualdisplay import Display
 
 def listAlphabet(isi: int):
   return list(string.ascii_uppercase)[isi]
@@ -33,6 +34,8 @@ def cari(soal: str, mapel: str, kodse_kelas: str):
 
     for list_quest in a["data"]["questions"]:
         if len(list_quest["options"]) == 0:
+            display = Display(visible=0, size(800,600))
+            display.start()
             imgkit.from_string('<p><b>Soal :</b></p>{}<p><b>Jawaban :</b></p>{}'.format(list_quest["contents"], list_quest['contentDefinition']), 'jawaban/{}.jpg'.format(list_quest['serial']))
             datajawaban.append('jawaban/{}.jpg'.format(list_quest['serial']))
         else:
@@ -41,6 +44,8 @@ def cari(soal: str, mapel: str, kodse_kelas: str):
             for all_answer in list_quest["options"]:
                 dumpopt += all_answer.replace("<p>", listAlphabet(abjad)+'. ').replace("<p style=\"text-align: justify;\">", listAlphabet(abjad)+'. ').replace("</p>", "<br>")
                 abjad += 1
+            display = Display(visible=0, size(800,600))
+            display.start()
             imgkit.from_string('<p><b>Soal :</b></p>{}{}<p><b>Jawaban :</b></p>{}'.format(list_quest["contents"],dumpopt, list_quest['contentDefinition']), 'jawaban/{}.jpg'.format(list_quest['serial']))
             datajawaban.append('jawaban/{}.jpg'.format(list_quest['serial']))
     return datajawaban
