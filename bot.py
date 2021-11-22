e8b34bc1fcb7954278f7934cb75faa9a9e183392
D-cat1 <72681912+D-cat1@users.noreply.github.com>
2021-11-22 21:01:03 +0700

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,88 @@
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+import pytesseract
+import random
+import string
+from roboguru import cari
+from time import sleep
+import os
+import imgkit
+
+client_robo = Client("hiyahiya",bot_token='1998370175:AAGBRDmRSGhHwlCSI95u_nZfx-_lHXoYZvk', api_id=1428215, api_hash='e7cc39008d3f5f1ee045f6b323c16786')
+
+def jenjang(jeneng_file):
+    return [[InlineKeyboardButton("SD", callback_data='jj 94LL64YTJA dig {}'.format(jeneng_file))],[InlineKeyboardButton("SMP", callback_data='jj XHAGLMC2TA dig {}'.format(jeneng_file))],[InlineKeyboardButton("SMA", callback_data='jj 3GAWQ3PJRB dig {}'.format(jeneng_file))],[InlineKeyboardButton("SBMPTN & STAN", callback_data='jj 489EUDXNA8 dig {}'.format(jeneng_file))]]
+
+def versi_sd(jeneng_file):
+    return [[InlineKeyboardButton("➗ Matematika", callback_data='mpl matematika {}'.format(jeneng_file))], [InlineKeyboardButton('🇮🇩 Bahasa Indonesia', callback_data='mpl bahasa%20indonesia {}'.format(jeneng_file))], [InlineKeyboardButton('🌱 IPA Terpadu', callback_data='mpl ipa%20terpadu {}'.format(jeneng_file))]]
+
+def versi_nonsd(jeneng_file):
+    return [[InlineKeyboardButton("➗ Matematika", callback_data='mpl matematika {}'.format(jeneng_file))], [InlineKeyboardButton('🇮🇩 Bahasa Indonesia', callback_data='mpl bahasa%20indonesia {}'.format(jeneng_file))], [InlineKeyboardButton('🌱 Biologi', callback_data='mpl biologi {}'.format(jeneng_file))], [InlineKeyboardButton('🍎 Fisika', callback_data='mpl fisika {}'.format(jeneng_file))], [InlineKeyboardButton('‍🔬 Kimia', callback_data='mpl kimia {}'.format(jeneng_file))], [InlineKeyboardButton('🇬🇧 Bahasa Inggris', callback_data='mpl bahasa%20inggris {}'.format(jeneng_file))], [InlineKeyboardButton('⏳ Sejarah', callback_data='mpl sejarah {}'.format(jeneng_file))], [InlineKeyboardButton('💵 Ekonomi', callback_data='mpl ekonomi {}'.format(jeneng_file))], [InlineKeyboardButton('🌏︎ Geografi', callback_data='mpl geografi {}'.format(jeneng_file))], [InlineKeyboardButton('🫂 Sosiologi', callback_data='mpl sosiologi {}'.format(jeneng_file))]]
+
+def randomize():
+    letters = string.ascii_letters
+    return ''.join(random.choice(letters) for i in range(3))
+
+@client_robo.on_message(filters.command('start'))
+def start(client, message):
+    #print(message)
+    client.send_message(message["chat"]["id"], """Hai {} 👋
+saya adalah bot penjawab soal
+kamu bisa memberikan soal kesaya melalui foto atapun text😊""".format(message["from_user"]["first_name"]))
+
+@client_robo.on_message(filters.command('tanya'))
+def cuaks(client, pesan):
+    if pesan.reply_to_message:
+        if pesan.reply_to_message.photo:
+            nama_file = randomize()
+            client_robo.download_media(pesan.reply_to_message.photo.file_id, file_name=nama_file+'.jpg')
+            client_robo.send_message(pesan.chat.id, "Silahkan pilih jenjang dengan mengklik tombol dibawah", reply_markup=InlineKeyboardMarkup(jenjang(nama_file+'.jpg')))
+            #ocr_text = pytesseract.image_to_string('downloads/{}.jpg'.format(pesan.reply_to_message.photo.file_unique_id), lang='eng')
+            #pesan.reply_text(ocr_text)
+        elif pesan.reply_to_message.document.mime_type == 'image/png':
+            nama_file = randomize()
+            client_robo.download_media(pesan.reply_to_message.document.file_id, file_name=nama_file+'.png')
+            client_robo.send_message(pesan.chat.id, "Silahkan pilih jenjang dengan mengklik tombol dibawah", reply_markup=InlineKeyboardMarkup(jenjang(nama_file+'.png')))
+        else:
+            print(False)
+    else:
+        if len(pesan.command) == 1:
+            client_robo.send_message(pesan.chat.id, "**Silahkan ketik pertanyaan atau foto pertanyaan lalu reply foto dengan command /tanya**")
+        else:
+            nama_file = randomize()
+            imgkit.from_string('<p>{}</p>'.format(pesan.text.split('/tanya ')[1]), 'downloads/{}.jpg'.format(nama_file))
+            client_robo.send_message(pesan.chat.id, "Silahkan pilih jenjang dengan mengklik tombol dibawah", reply_markup=InlineKeyboardMarkup(jenjang(nama_file+'.jpg')))
+
+@client_robo.on_callback_query()
+def hailah(client, pesan):
+    commandfin = pesan.data.split(' ')[0]
+    if commandfin == 'jj':
+       selector = pesan.data.split(' ')[1]
+       if selector == '94LL64YTJA':
+           client_robo.delete_messages(pesan.message.chat.id, pesan.message.message_id)
+           client_robo.send_message(pesan.message.chat.id, "Silahkan pilih mapel dengan mengklik tombol dibawah", reply_markup=InlineKeyboardMarkup(versi_sd(pesan.data)))
+       elif selector == 'XHAGLMC2TA':
+           client_robo.delete_messages(pesan.message.chat.id, pesan.message.message_id)
+           client_robo.send_message(pesan.message.chat.id, "Silahkan pilih mapel dengan mengklik tombol dibawah", reply_markup=InlineKeyboardMarkup(versi_nonsd(pesan.data)))
+       elif selector == '3GAWQ3PJRB':
+           client_robo.delete_messages(pesan.message.chat.id, pesan.message.message_id)
+           client_robo.send_message(pesan.message.chat.id, "Silahkan pilih mapel dengan mengklik tombol dibawah", reply_markup=InlineKeyboardMarkup(versi_nonsd(pesan.data)))
+       elif selector == '489EUDXNA8':
+           client_robo.delete_messages(pesan.message.chat.id, pesan.message.message_id)
+           client_robo.send_message(pesan.message.chat.id, "Silahkan pilih mapel dengan mengklik tombol dibawah", reply_markup=InlineKeyboardMarkup(versi_nonsd(pesan.data)))
+    elif commandfin == 'mpl':
+        client_robo.delete_messages(pesan.message.chat.id, pesan.message.message_id)
+        namefile = pesan.data.split('dig ')[1]
+        mapela = pesan.data.split('mpl ')[1].split(' ')[0]
+        kode_kelas = pesan.data.split('jj ')[1].split(' ')[0]
+        ocr_text = pytesseract.image_to_string('downloads/{}'.format(namefile), lang='eng')
+        aaaaa = client_robo.send_message(pesan.message.chat.id, "Harap tunggu.. Mencari soal yang mirip 🔍")
+        aings = cari(ocr_text, mapela, kode_kelas)
+        client_robo.edit_message_text(aaaaa.chat.id, aaaaa.message_id, 'Ditemukan {} soal yang mirip\n__Mengirim jawaban  📃__'.format(len(aings)))
+        for aing in aings:
+            client_robo.send_photo(pesan.message.chat.id, aing)
+            sleep(3)
+            os.remove(aing)
+
+
+client_robo.run()
---
+++ roboguru.py
@@ -0,0 +1,49 @@
+import json
+import requests as robo_ribet
+import urllib.parse as asik
+import html2text
+import imgkit
+import string
+
+def listAlphabet(isi: int):
+  return list(string.ascii_uppercase)[isi]
+
+def cari(soal: str, mapel: str, kodse_kelas: str):
+    datajawaban = []
+    header = {"accept": "application/json",
+    "accept-encoding": "gzip, deflate, br",
+    "accept-language": "id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7,ja;q=0.6",
+    "cache-control": "no-cache",
+    "content-type": "application/json",
+    "country": "id",
+    "sec-ch-ua": '"Chromium";v="92", " Not A;Brand";v="99", "Google Chrome";v="92"',
+    "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36",
+    "with-auth": "true"}
+    h = html2text.HTML2Text()
+    h.ignore_images = True
+    asri = robo_ribet.get('https://roboguru.ruangguru.com', headers=header)
+    a = robo_ribet.get('https://roboguru.ruangguru.com/api/v3/roboguru-discovery/search/question?gradeSerial={}&subjectName={}&withVideo=true&text={}&imageURL=&singleQuestion=false'.format(kodse_kelas, mapel ,asik.quote(soal)), headers=header, cookies=asri.cookies).json()
+
+    #print('Soal : ' +h.handle(a["data"]["questions"][0]["contents"]))
+    #print('Link Ke Soal & Jawaban : https://roboguru.ruangguru.com/question/{}'.format(a["data"]["questions"][0]["serial"]))
+    #print('Jawab : '+h.handle(a["data"]["questions"][0]["contentDefinition"]))
+
+    #with open('data.json', 'w') as f:
+    #    json.dump(a, f)
+
+    for list_quest in a["data"]["questions"]:
+        if len(list_quest["options"]) == 0:
+            imgkit.from_string('<p><b>Soal :</b></p>{}<p><b>Jawaban :</b></p>{}'.format(list_quest["contents"], list_quest['contentDefinition']), 'jawaban/{}.jpg'.format(list_quest['serial']))
+            datajawaban.append('jawaban/{}.jpg'.format(list_quest['serial']))
+        else:
+            dumpopt = ''
+            abjad = 0
+            for all_answer in list_quest["options"]:
+                dumpopt += all_answer.replace("<p>", listAlphabet(abjad)+'. ').replace("<p style=\"text-align: justify;\">", listAlphabet(abjad)+'. ').replace("</p>", "<br>")
+                abjad += 1
+            imgkit.from_string('<p><b>Soal :</b></p>{}{}<p><b>Jawaban :</b></p>{}'.format(list_quest["contents"],dumpopt, list_quest['contentDefinition']), 'jawaban/{}.jpg'.format(list_quest['serial']))
+            datajawaban.append('jawaban/{}.jpg'.format(list_quest['serial']))
+    return datajawaban
+        
+    
+
