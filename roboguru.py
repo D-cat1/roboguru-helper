fb6f869104d8e458e4726636fe89d6591f72ca0c
D-cat1 <72681912+D-cat1@users.noreply.github.com>
2021-11-22 23:27:36 +0700

    Update roboguru.py

--- roboguru.py
+++ roboguru.py
@@ -33,7 +33,7 @@ def cari(soal: str, mapel: str, kodse_kelas: str):
 
     for list_quest in a["data"]["questions"]:
         if len(list_quest["options"]) == 0:
-            imgkit.from_string('<p><b>Soal :</b></p>{}<p><b>Jawaban :</b></p>{}'.format(list_quest["contents"], list_quest['contentDefinition']), 'jawaban/{}.jpg'.format(list_quest['serial']), options={"xvfb": "", "format": "jpg"})
+            imgkit.from_string('<p><b>Soal :</b></p>{}<p><b>Jawaban :</b></p>{}'.format(list_quest["contents"], list_quest['contentDefinition']), 'jawaban/{}.jpg'.format(list_quest['serial']))
             datajawaban.append('jawaban/{}.jpg'.format(list_quest['serial']))
         else:
             dumpopt = ''
@@ -41,7 +41,7 @@ def cari(soal: str, mapel: str, kodse_kelas: str):
             for all_answer in list_quest["options"]:
                 dumpopt += all_answer.replace("<p>", listAlphabet(abjad)+'. ').replace("<p style=\"text-align: justify;\">", listAlphabet(abjad)+'. ').replace("</p>", "<br>")
                 abjad += 1
-            imgkit.from_string('<p><b>Soal :</b></p>{}{}<p><b>Jawaban :</b></p>{}'.format(list_quest["contents"],dumpopt, list_quest['contentDefinition']), 'jawaban/{}.jpg'.format(list_quest['serial']), options={"xvfb": "", "format": "jpg"})
+            imgkit.from_string('<p><b>Soal :</b></p>{}{}<p><b>Jawaban :</b></p>{}'.format(list_quest["contents"],dumpopt, list_quest['contentDefinition']), 'jawaban/{}.jpg'.format(list_quest['serial']))
             datajawaban.append('jawaban/{}.jpg'.format(list_quest['serial']))
     return datajawaban
         
